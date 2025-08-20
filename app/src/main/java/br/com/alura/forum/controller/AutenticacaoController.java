c401c578b58538c77dc3aa8a527070aa6d95fc35
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2025-08-20 18:31:58 -0300

    feat: prometheus, nginx and mysql

--- app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,7 +1,6 @@
 package br.com.alura.forum.controller;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +12,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.controller.dto.TokenDto;
 import br.com.alura.forum.controller.form.LoginForm;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @RestController
 @RequestMapping("/auth")
 @Profile(value = {"prod", "test"})
 public class AutenticacaoController {
 	
+	Counter authUserSuccess;
+	Counter authUserErrors;
+
+	public AutenticacaoController(MeterRegistry registry) {
+    	authUserSuccess = Counter.builder("auth_user_success")
+            .description("usuarios autenticados")
+            .register(registry);
+    	
+    	authUserErrors = Counter.builder("auth_user_error")
+                .description("erros de login")
+                .register(registry);
+    }
+    
 	@Autowired
 	private AuthenticationManager authManager;
 	
@@ -35,13 +48,13 @@ public class AutenticacaoController {
 		
 		try {
 			Authentication authentication = authManager.authenticate(dadosLogin);
-			String token = tokenService.gerarToken(authentication); 		
+			String token = tokenService.gerarToken(authentication);
+			authUserSuccess.increment();
 			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
 			
 		} catch (AuthenticationException e) {
+			authUserErrors.increment();
 			return ResponseEntity.badRequest().build();
-		}
-
-		
+		}	
 	}
 }
